407a49daaf3aefad3e74125e5b1c9097f37ab867
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-12-09 22:24:51 +0330

    Seperate my apps from django default apps and introduce 'accounts' app to django project by adding that into Installed_apps list

--- backend/settings.py
+++ backend/settings.py
@@ -25,12 +25,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+INSTALLED_APPS += [
+    # My apps
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
