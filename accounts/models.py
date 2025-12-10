b7fcd454a3d1391135488fce04a06cef35c93f3c
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-12-10 21:44:03 +0330

    Add custom user model with email as username and is_verified field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from accounts.managers import CustomUserManager
 
-# Create your models here.
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+    def __str__(self):
+        return self.email
--- backend/settings.py
+++ backend/settings.py
@@ -115,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# My settings
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
