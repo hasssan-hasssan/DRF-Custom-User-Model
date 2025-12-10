da08346fcf0cb36aa3ff42a3b876f7311eb79632
Abolfazl Hassan Zade <abolfazl.hassanzade.81@gmail.com>
2025-12-10 22:14:46 +0330

    Register and custom admin panel of custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from accounts.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+
+    model = CustomUser
+
+    list_display = ('email', 'full_name', 'is_staff',
+                    'is_verified', 'is_active')
+    list_filter = ('is_staff', 'is_verified', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('full_name',)}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active',
+         'is_verified', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2', 'is_staff', 'is_active', 'is_verified')}
+         ),
+    )
+
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
--- accounts/managers.py
+++ accounts/managers.py
@@ -14,4 +14,6 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verified', True)
         return self.create_user(email, password, **extra_fields)
